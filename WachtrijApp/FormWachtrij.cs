86f7c66e81cdd8a7ff71bc9cb768e9c3d60a0214
FlorisvanBezooijen <Floorftw@gmail.com>
2021-02-25 10:26:10 +0100

    Status "In onderhoud" toegevoegd

--- WachtrijApp/FormWachtrij.cs
+++ WachtrijApp/FormWachtrij.cs
@@ -140,6 +140,10 @@ namespace WachtrijApp
             {
                 return "Komt binnen";
             }
+            if (StatusNr == "5")
+            {
+                return "In onderhoud";
+            }
 
             return "";
         }
